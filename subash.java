39527831a9aca816c4f1044f4c6c43305cee9130
subashdhungana123 <dhunganas761@gmail.com>
2021-10-18 14:28:33 -0500

    try again

--- subash.java
+++ subash.java
@@ -1,5 +1,6 @@
 public class subash {
     public static void main(String[] args) {
         System.out.println("My name is subash");
+        System.out.println("Thank you for your service");
     }
 }
